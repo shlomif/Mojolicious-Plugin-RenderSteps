730d8d8989bd0382bea7e0727920881b5ac564bb
Shlomi Fish <shlomif@shlomifish.org>
2015-05-01 13:19:16 +0300

    Fix tests on recent Mojolicious - requiring 6.10

--- lib/Mojolicious/Plugin/RenderSteps.pm
+++ lib/Mojolicious/Plugin/RenderSteps.pm
@@ -12,7 +12,7 @@ sub register {
       $self->render_later;
       $self->stash->{'rendersteps.depth'}++;
       my $delay = Mojo::IOLoop->delay(@steps);
-      $delay->on(error => sub { $self->render_exception(pop) });
+      $delay->on(error => sub { $self->helpers->reply->exception(pop) });
       $delay->on(
         finish => sub {
           my $delay = shift;
